a7c06a3d143dbbe63e675671b393f5d4b6b80584
ProFrenchToast <66527432+ProFrenchToast@users.noreply.github.com>
2025-08-29 14:10:15 +0100

    added abstract narrative understanding task

---
+++ Evaluations/Abstract_Narrative_Understanding/__init__.py
@@ -0,0 +1,5 @@
+"""Abstract Narrative Understanding evaluation task."""
+
+from .abstract_narrarive_understanding_task import record_to_sample, abstract_narrative_understanding_task
+
+__all__ = ["record_to_sample", "abstract_narrative_understanding_task"]
