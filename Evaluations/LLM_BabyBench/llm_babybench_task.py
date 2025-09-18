33626eb4367eb18372b38ee33cfabfbe9dbb340a
ProFrenchToast <66527432+ProFrenchToast@users.noreply.github.com>
2025-09-18 15:13:13 +0100

    added decompose task to llm_babybench

--- Evaluations/LLM_BabyBench/llm_babybench_task.py
+++ Evaluations/LLM_BabyBench/llm_babybench_task.py
@@ -60,6 +60,33 @@ def plan_task() -> Task:
                 scorer=[match(), plan_scorer()],
                 )
 
+def decompose_record_to_sample(record: Dict[str, Any]) -> Sample:
+    input = f"{record['env_description']} What subgoals should be taken in order to achive the mission?:\n initial_state ((x, y), d) where d is the direction the agent is facing (east=0, south=1, west=2, north=3): {record['initial_state']}\n mission: {record['mission']} \n give your answer list of subgoals each on a new line. sub goals should be in the format and are executed in the order you give: "
+    input += """- OpenSubgoal: If the agent is next to a closed door, this subgoal will open it. \n\n
+- CloseSubgoal: If the agent is next to an open door, this subgoal will close it. \n\n
+- DropSubgoal: If the agent is carrying an object, this subgoal drops it. \n\n
+- PickupSubgoal: If the agent is not carrying any object and is next to an object, this subgoal picks it up. \n\n
+- GoNextToSubgoal, (x,y): If there is a clear, without any blocker, path between the agent and the cell of coordinates (x,y), this subgoal makes the agent go next to this cell."""
+    sample = Sample(input=input,
+                    metadata=record,
+                    )
+    return sample
+
+@scorer(metrics=[accuracy(), stderr()])
+def decompose_scorer()-> Scorer:
+    """This is a task specific scorer that will check if the action sequence ends up in the correct location."""
+    async def score(state: TaskState, target: Target) -> Score:
+        return Score(value="C")
+    return score
+
 @task
 def decompose_task() -> Task:
-    pass
+    dataset = hf_dataset("salem-mbzuai/LLM-BabyBench",
+                         name="decompose",
+                         split="train",
+                         sample_fields=decompose_record_to_sample
+                         )
+    return Task(dataset=dataset,
+                solver=generate(),
+                scorer=decompose_scorer(),
+                )
