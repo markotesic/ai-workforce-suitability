0c24a33dae7957feec539ececb8cb0cc8fb20f0d
ProFrenchToast <66527432+ProFrenchToast@users.noreply.github.com>
2025-09-24 00:51:24 +0100

    removed max_connections

--- Evaluations/EWoK/ewok_task.py
+++ Evaluations/EWoK/ewok_task.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     dataset = dataset[:num_samples]
 
     annotation_task = annotate_task(dataset)
-    log = eval(annotation_task, model="openai/azure/gpt-4o", max_connections=2)
+    log = eval(annotation_task, model="openai/azure/gpt-4o" )
     extract_annotations(log[0], output_path)
 
 
